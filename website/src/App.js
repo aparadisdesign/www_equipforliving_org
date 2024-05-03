9c9c09e5c04a49f6b7052e9b02f91918111e9a53
Brandon H. Goding <brandon.h.goding@gmail.com>
2024-05-03 16:29:55 -0400

    feature(restore): restore

--- website/src/App.js
+++ website/src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navigation from "./components/Navigation";
-import ProgramCards from "./components/ProgramCards";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import Programs from "./pages/Programs";
@@ -9,10 +7,10 @@ import EventRegistration from "./pages/EventRegistration";
 import NoPage from "./pages/NoPage";
 
 const navigation = [
-  // { name: 'Events', href: '#' },
+  { name: 'Events', href: '/events' },
   { name: 'Programs', href: '/programs' },
   { name: 'Donate', href: 'https://www.paypal.com/donate/?cmd=_donations&business=equipforliving@gmail.com&item_name=Equip%20For%20Living%20Foundation&currency_code=USD' },
-  // { name: 'Contact Us', href: '#' },
+  { name: 'Contact Us', href: '#' },
 ]
 
 function App() {
@@ -21,8 +19,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home navigation={navigation} />} />
-          {/* <Route path="events" element={<Events navigation={navigation} />} /> */}
-          {/* <Route path="event-sign-up" element={<EventRegistration navigation={navigation} />} /> */}
+           <Route path="events" element={<Events navigation={navigation} />} />
+           <Route path="event-sign-up" element={<EventRegistration navigation={navigation} />} />
           <Route path="programs" element={<Programs navigation={navigation} />} />
           <Route path="*" element={<NoPage />} />
         </Route>
